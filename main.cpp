ea67f38f12abac796398642c559fe925e5786f96
simsgood2 <simsgood2@gmail.com>
2022-09-17 17:36:21 +0900

    practice2

--- main.cpp
+++ main.cpp
@@ -2,11 +2,37 @@
 
 using namespace std;
 
+void sort(int &x, int &y, int &z){
 
-int main() {
-    cout << "hello";
+    int small=x;
+    int big=x;
 
-    cout<<" sdj;lf;safja";
+    if(small>y) small=y;
+    if(big<y) big=y;
+    if(small>z) small=z;
+    if(big<z) big=z;
 
-    cout<<"sd;jfasfdjsa;dfa";
+    int middle;
+    if(x>small&&x<big) middle=x;
+    if(y>small&&y<big) middle=y;
+    if(z>small&&z<big) middle=z;
+
+    x=small;
+    y=middle;
+    z=big;
+
+}
+
+int main( ) {
+    int x, y, z;
+    char ans;
+    do {
+        cout << "Enter three integers: ";
+        cin >> x >> y >> z;
+        sort(x, y, z);
+        cout << x << " " << y << " " << z << endl;
+        cout << "Again? (Y/y) or type any others to quit: ";
+        cin >> ans;
+    } while (ans == 'y' || ans == 'Y');
+    return 0;
 }
